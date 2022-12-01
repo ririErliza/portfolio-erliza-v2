1f8395072c7802452bd6c065d840f66e7a59f57b
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-12-01 11:30:35 +0100

    update

--- components/Cards.jsx
+++ components/Cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
   return (
     <>
     <div className="mb-10 flex flex-col items-center bg-gray-100 rounded-lg shadow-lg md:flex-row md:max-w-3xl hover:bg-white dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <Image className='w-full rounded-t-lg h-full md:h-auto md:w-48 md:rounded-none md:rounded-l-lg' src={Introduce} alt='/' objectFit='fill' />
+        <Image className='hover:opacity-50 w-full rounded-t-lg h-full md:h-auto md:w-48 md:rounded-none md:rounded-l-lg' src={Introduce} alt='/' objectFit='fill' />
         <div className='flex flex-col justify-between p-4 leading-normal'>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         </div>
@@ -19,7 +19,7 @@ const Cards = () => {
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
         </div>
 
-        <Image className='w-full rounded-b-lg h-full md:h-auto md:w-48 md:rounded-none md:rounded-r-lg' src={Education} alt='/' objectFit='fill' />
+        <Image className='hover:opacity-50 w-full rounded-b-lg h-full md:h-auto md:w-48 md:rounded-none md:rounded-r-lg' src={Education} alt='/' objectFit='fill' />
         
     </div>
 
