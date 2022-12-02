d58a718d5c8751233338e8ccfcc4ba689f2f50a4
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-12-02 15:17:39 +0100

    update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -64,7 +64,7 @@ const Hero = () => {
                 </div>
 
 
-                <div className='absolute top-[12rem] bg-[#8cbbd6] w-[380px]  xl:h-[200px] h-[100px] blur-3xl -z-10'></div>
+                <div className='absolute top-[12rem] bg-[#8cbbd6] w-[380px]  xl:h-[200px] h-[150px] blur-3xl -z-10'></div>
       
 
     </div>
